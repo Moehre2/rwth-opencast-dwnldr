e31e7b735e3d9440afb6b780f27015953b3b1007
Moritz <cookie.moehre2@gmail.com>
2020-10-11 18:41:37 +0200

    Video downloader gets contents of playlist.m3u8 perfectly

--- video_downloader/__init__.py
+++ video_downloader/__init__.py
@@ -1,8 +1,10 @@
 # (c) 2020 Moehre2
 
 import os.path
+import http.client
 
 outputfolder = ""
+playlist_buffer = []
 
 def init(folder):
     global outputfolder
@@ -15,10 +17,29 @@ def check_folder(folder_name):
     else:
         os.makedirs(folder_name)
 
+def get_playlist(output, guid):
+    global playlist_buffer
+    conn = http.client.HTTPSConnection("streaming.rwth-aachen.de")
+    conn.request("GET", "/rwth/smil:engage-player_" + guid + "_presentation.smil/playlist.m3u8")
+    req = conn.getresponse()
+    success = (req.status == 200)
+    if not success:
+        playlist_buffer = []
+        print(req.status, req.reason)
+    else:
+        data = req.read()
+        playlist_buffer = data.split(b"\n")
+        print(playlist_buffer)
+        print("=> playlist.m3u8")
+    conn.close()
+    return success
+
 def download(video):
     global outputfolder
     print("Download:", video["name"])
     if outputfolder[len(outputfolder) - 1:] == "/":
-        check_folder(outputfolder + video["name"])
+        output = outputfolder + video["name"]
     else:
-        check_folder(outputfolder + "/" + video["name"])
+        output = outputfolder + "/" + video["name"]
+    check_folder(output)
+    get_playlist(output, video["guid"])
