1dcdc5570c6c5d09af5f2b331c1f21ac4c65b5b7
Moritz <cookie.moehre2@gmail.com>
2020-10-11 16:57:32 +0200

    Added file checks

--- rwth-opencast-dwnldr.py
+++ rwth-opencast-dwnldr.py
@@ -1,7 +1,24 @@
 # (c) 2020 Moehre2
 
+import sys
+import os.path
+
+inputfile = "";
+
 def main():
     print("rwth-opencast-dwnldr 0.1")
+    if len(sys.argv) == 1:
+        inputfile = input("Please enter a file: ")
+    elif len(sys.argv) == 2:
+        inputfile = sys.argv[1]
+    else:
+        print("Error: Unknown parameters...")
+        sys.exit(1)
+    if os.path.exists(inputfile) and os.path.isfile(inputfile):
+       print("Yeah")
+    else:
+        print("Error: Invalid file")
+        sys.exit(2)
 
 try:
     main()
